61c8bb833290552f842ce449e1f43a9fcabe4629
mikeiannotti55 <79390433+mikeiannotti55@users.noreply.github.com>
2022-11-27 19:36:03 -0600

    fiexd logo butotn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <a  href='/'  >
+        <a  href='/mikesportfolio/'  >
           <img type="button" className='cursor-pointer' hre src={Logo3} alt='Logo Image' style={{ width: '80px' }} />
         </a>
       </div>
