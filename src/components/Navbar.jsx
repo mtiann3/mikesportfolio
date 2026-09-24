b5f69ca214b74fdb2243ba5f1398f6e08189b120
mtiann3 <mtiann3@gmail.com>
2026-09-24 11:47:05 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
   const navLinks = [
     { to: "home", label: "Home" },
     { to: "about", label: "About" },
-    { to: "focus", label: "Focus Areas" },
+    { to: "focus", label: "Focus" },
     { to: "experience", label: "Experience" },
     { to: "education", label: "Education" },
     { to: "projects", label: "Projects" },
